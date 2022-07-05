21df13200c8977dd2009a6316a9d90e8c76fed5a
Alireza Safari <alireza.safaree@gmail.com>
2022-07-05 21:30:01 +0430

    style(statics): adds statics / medias
    - adds STATIC_ROOT
    - adds STATIC_URL
    - adds MEDIA_ROOT
    - adds MEDIA_URL

--- singl_page/urls.py
+++ singl_page/urls.py
@@ -14,5 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from portfolio.urls import urlpatterns as portfolio_urls
+from django.conf.urls.static import static
+from django.conf import settings
 
-urlpatterns = [] + portfolio_urls
+urlpatterns = []
+urlpatterns += portfolio_urls
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
