7868dff25c4e6f4a4ce95779a505a2c59357bf8a
Alireza Safari <alireza.safaree@gmail.com>
2022-07-05 23:30:05 +0430

    build(settings): adds heroku to allowed hosts

--- single_page/settings.py
+++ single_page/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-43tc*66!+gjahh40+i0t0m8d336&f9&)r(%dftw*md!5qe@t6='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-single-page.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
