ebb09ff8b59409a808f054154fa7293f178ada5c
Alireza Safari <alireza.safaree@gmail.com>
2022-07-05 23:56:54 +0430

    build(static): moves avatar to static to use in build

--- single_page/settings.py
+++ single_page/settings.py
@@ -120,10 +120,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'statics'
-]
-
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
